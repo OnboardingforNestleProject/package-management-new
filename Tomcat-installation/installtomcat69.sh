7d03064ca17041a99fe432492b146916b94ce7dd
Uzoma Emeka Kingsley <98700329+Uzomaemekakingsley@users.noreply.github.com>
2024-05-08 09:51:01 +0100

    Update installtomcat69.sh

--- Tomcat-installation/installtomcat69.sh
+++ Tomcat-installation/installtomcat69.sh
@@ -10,7 +10,7 @@ sudo yum install java-1.8.0-openjdk-devel -y
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.23/bin/apache-tomcat-10.1.23.tar.gz
 sudo tar -xvf apache-tomcat-10.1.23.tar.gz
 sudo rm apache-tomcat-10.1.23.tar.gz
-sudo mv apache-tomcat-10.1.23.tar.gz tomcat9
+sudo mv apache-tomcat-10.1.23 tomcat9
 sudo chmod 777 -R /opt/tomcat9
 sudo sh /opt/tomcat9/bin/startup.sh
 # create a soft link to start and stop tomcat from anywhere
